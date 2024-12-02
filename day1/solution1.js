a8fa2579f3214fb1bcb1c5b46f005376d2df495f
Danek <danek.fill@outlook.com>
2024-12-02 14:08:47 -0500

    day 1 initial setup

---
+++ day1/solution1.js
@@ -0,0 +1,47 @@
+/*
+- list must find the difference betweent the smallest in left list to smallest in right list
+- add to new "distance" list
+- repeat for 2n3, 3rd, 4th... smallest numbers until list is complete
+
+- return total distance 
+*/
+
+// 1.read file input
+const { error } = require('console');
+const fs = require('fs');
+const input = fs.readFileSync('input1.txt', (err, data) => {
+  if (err) throw err;
+  return data.toString();
+});
+
+const data = input.toString();
+
+// 2.separate into two lists
+const list1 = [];
+const list2 = [];
+
+  //error handling of mismatched sizes
+  if (list1.length != list2.length) {
+    throw new Error ('list lengths do not match, please check input file');
+  }
+
+  //separate data into two lists, delimited by (3 spaces && new lines)
+
+
+
+// 3. find distance between min of each list, over and over until done
+
+const sortedList1 = list1.sort((a,b) => a - b);
+const sortedList2 = list2.sort((a,b) => a - b);
+const lengthList = [];
+
+for (let i = 0; i < sortedList1.length; i++) {
+  const distance = Math.abs(sortedList1[i] - sortedList2[i])
+  lengthList.push(distance);
+};
+
+// 4. find total distance
+
+const totalDistance = lengthList.reduce((acc, distance) => acc + distance, 0);
+
+console.log(totalDistance);
